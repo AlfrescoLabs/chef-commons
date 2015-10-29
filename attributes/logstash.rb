78394d2655b7effbc4fe15af129c409db0110003
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-10-29 22:13:41 +0100

    fix logstash cron

--- attributes/logstash.rb
+++ attributes/logstash.rb
@@ -1,3 +1,4 @@
 default['genius']['logstash']['discovery-chef-json'] = "/etc/chef/logstash-discovery.json"
 default['genius']['logstash']['ec2_role_name'] = 'logstash'
 default['genius']['logstash']['run_discovery'] = false
+default['genius']['logstash']['discovery-cron'] = "*/5 * * * *"
