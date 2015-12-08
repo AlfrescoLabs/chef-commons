4659c51a6a7015cd14795a97187b350b10764dfc
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-08 23:38:17 +0100

    added filter in/out

--- attributes/ec2-discovery.rb
+++ attributes/ec2-discovery.rb
@@ -4,20 +4,18 @@ default['commons']['ec2-discovery']['wget_command'] = 'wget'
 # Used for local testing
 default['commons']['ec2-discovery']['skip_ec2_commands'] = false
 
-default['commons']['ec2-discovery']['peers_file_path'] = "/etc/chef/ec2-peers.json"
-default['commons']['ec2-discovery']['current_file_path'] = "/etc/chef/ec2-current.json"
-
 default['commons']['ec2-discovery']['output']['elements']['state'] = 'State/Name'
 default['commons']['ec2-discovery']['output']['elements']['id'] = 'InstanceId'
 default['commons']['ec2-discovery']['output']['elements']['ip'] = "PrivateIpAddress"
 default['commons']['ec2-discovery']['output']['elements']['az'] = 'Placement/AvailabilityZone'
-default['commons']['ec2-discovery']['output']['tags']['role'] = 'haproxy-role'
+default['commons']['ec2-discovery']['output']['tags']['roles'] = 'roles'
 default['commons']['ec2-discovery']['output']['tags']['instance_name'] = 'Name'
 
-default['commons']['ec2-discovery']['filter_out']['ip']['current_attribute'] = "local-ipv4"
-default['commons']['ec2-discovery']['filter_in']['state']['value'] = "running"
+default['commons']['ec2-discovery']['group_by'] = ['roles','az','id']
 
-default['commons']['ec2-discovery']['group_by'] = ['role','az']
+default['commons']['ec2-discovery']['filter_in']['state'] = "running"
+
+default['commons']['ec2-discovery']['filter_out']['current_ip'] = true
 
 # default['commons']['ec2-discovery']['query_tags']['status'] = "complete"
 # default['commons']['ec2-discovery']['query_tags']['stack_name'] = "mystack"
--- libraries/chef_commons_ec2_discovery.rb
+++ libraries/chef_commons_ec2_discovery.rb
@@ -1,164 +1,168 @@
 
 class Chef
   module Ec2Discovery
-
-
     class << self
-
       include Chef::Mixin::ShellOut
 
       def discover(config)
-        aws_bin = config['aws_command']
-        wget_bin = config['wget_command']
-        ec2_peers_file_path = config['peers_file_path']
-        ec2_current_file_path = config['current_file_path']
-        query_tags = config['query_tags']
-        skip_ec2_commands = config['skip_ec2_commands']
-        puts " [EC2 Discovery] Running...\n"
+        # Cloning, loading and parsing configuration
+        cfg = config.to_hash.clone
+        aws_bin = cfg['aws_command']
+        wget_bin = cfg['wget_command']
+        query_tags = cfg['query_tags']
+        skip_ec2_commands = cfg['skip_ec2_commands']
+        group_by = cfg['group_by']
+        filter_in = cfg['filter_in']
+        filter_out = cfg['filter_out']
+        query_tag_filter = getAwsQueryFilter(config['query_tags'])
+        output = {}
 
+        # If current instance params are provided, there's no need
+        # to invoke aws command
+        if cfg['current']
+          current_ip = cfg['current']['ip']
+          current_az = cfg['current']['az']
+          ec2_peers = File.read('/etc/chef/ec2-peers.json')
+        else
+          current_ip = getAwsMetadata(wget_bin, 'local-ipv4')
+          current_az = getAwsMetadata(wget_bin, 'placement/availability_zone')
+          ec2_peers = run_cmd("#{aws_bin} ec2 describe-instances #{query_tag_filter}")
+        end
+        puts " [EC2 Discovery] Start!\n"
+        puts "[EC2 Discovery] Current ip: #{current_ip}\n"
+        puts "[EC2 Discovery] Current az: #{current_az}\n"
+
+        ec2_peers_hash = JSON.parse(ec2_peers)
+        # puts " [EC2 Discovery] Loaded ec2_peers JSON #{ec2_peers_hash} and Config #{config}'\n"
+
+        if ec2_peers_hash['Reservations'] and ec2_peers_hash['Reservations'].length > 0
+          ec2_peers_hash['Reservations'].each do |reservation|
+            reservation['Instances'].each do |awsnode|
+              instance_details = {}
+
+              # Collect element values
+              cfg['output']['elements'].each do |element_name,element_value|
+                instance_details[element_name] = getSubAttributeStr(awsnode,element_value)
+              end
+
+              # Collect tag values
+              cfg['output']['tags'].each do |tag_name,tag_value|
+                instance_details[tag_name] = getTagValue(awsnode['Tags'], tag_value)
+              end
+
+              # Filter in instances
+              filtered_in = true
+              filter_in.each do |field_name,field_value|
+                filtered_in = false if instance_details[field_name] != field_value
+              end
+
+              # TODO - not working
+              # Filter out instances
+              filtered_out = false
+              filter_out.each do |field_name,field_value|
+                if field_name == 'current_ip'
+                  field_name = 'ip'
+                  field_value = current_ip
+                end
+                filtered_out = true if instance_details[field_name] == field_value
+              end
+
+              # Add instance to the output list, if not filtered
+              if filtered_in and !filtered_out
+                setFacetAttribute(output,group_by,instance_details)
+                puts "[EC2 Discovery] Registered EC2 instance #{instance_details}\n"
+              else
+                puts "[EC2 Discovery] Filtered out EC2 instance #{instance_details}\n"
+              end
+            end
+          end
+        end
+        puts "[EC2 Discovery] DEBUG: Returning output \n#{JSON.pretty_generate(output)}\n"
+        return output
+      end
+
+      private
+
+      def run_cmd(command)
+        cmd = shell_out!(command)
+        return cmd.stdout
+      end
+
+      def getAwsMetadata(wget_bin, item)
+         return run_cmd("#{wget_bin} -q -O - http://169.254.169.254/latest/meta-data/#{item}")
+      end
+
+      def getAwsQueryFilter(query_tags)
         query_tag_filter = ""
         if query_tags
           query_tags.each do |tagName,tagValue|
             query_tag_filter += "--filters Name=tag:#{tagName},Values=#{tagValue} "
           end
         end
-
-        # Create ec2 files, current and peers
-        run_cmd("#{aws_bin} ec2 describe-instances #{query_tag_filter} > #{ec2_peers_file_path}") unless skip_ec2_commands
-        ec2_peers_file = JSON.parse(File.read(ec2_peers_file_path))
-        puts " [EC2 Discovery] Loaded ec2_peers JSON:\n#{ec2_peers_file}'\n"
-
-        run_cmd("#{wget_bin} -q -O - http://169.254.169.254/latest/meta-data > #{ec2_current_file_path}") unless skip_ec2_commands
-        ec2_current_file = JSON.parse(File.read(ec2_current_file_path))
-        puts " [EC2 Discovery] Loaded ec2_current JSON:\n#{ec2_current_file}'\n"
-
-        puts " [EC2 Discovery] Parsing EC2 instances using config:\n#{config}'\n"
-        output = {}
-
-        if ec2_peers_file['Reservations'] and ec2_peers_file['Reservations'].length > 0
-          puts " [EC2 Discovery] Found EC2 #{ec2_peers_file['Reservations'].size()} instances\n"
-          ec2_peers_file['Reservations'].each do |reservation|
-            reservation['Instances'].each do |awsnode|
-              config['output']['elements'].each do |elementName,elementValue|
-                output[elementName] = getSubAttribute(awsnode,elementValue)
-                puts " [EC2 Discovery] Writing output element #{elementName}=#{output[elementName]}\n"
-              end
-              config['output']['tags'].each do |tagName,tagValue|
-                value = ""
-                awsnode['Tags'].each do |tag|
-                  if tag['Key'] == tagValue
-                    value = tag['Value']
-                  end
-                end
-                output[tagName] = value
-                puts " [EC2 Discovery] Writing output tag #{tagName}=#{value}\n"
-              end
-
-              # TODO - ...
-            end
-          end
-        end
-        puts " [EC2 Discovery] Returning output #{output}\n"
-        return output
-
-
-        #     private_ip = awsnode['PrivateIpAddress']
-        #     availability_zone = awsnode['Placement']['AvailabilityZone']
-        #     status = awsnode['State']['Name']
-        #     id = awsnode['InstanceId']
-        #     Chef::Log.info("Parsing EC2 instance '#{id}', status '#{status}', avaliability zone '#{availability_zone}', private IP '#{private_ip}'")
-        #
-        #     role = ""
-        #     instanceName = ""
-        #
-        #     if status == "running"
-        #
-        #       # 1. Collect Name and role_tag values from AWS tags
-        #       awsnode['Tags'].each do |tag|
-        #         if tag['Key'] == "Name"
-        #           instanceName = tag['Value']
-        #         elsif tag['Key'] == role_tag_name
-        #           role = tag['Value']
-        #         end
-        #       end
-        #
-        #       # Define node and haproxy backend configuration
-        #       Chef::Log.info("EC2 instance '#{id}' has role '#{role}'")
-        #       haproxy_backends[role] = {} unless haproxy_backends[role]
-        #
-        #       haproxy_backends[role]['zones'] = {} unless haproxy_backends[role]['zones']
-        #
-        #       unless haproxy_backends[role]['zones'][availability_zone]
-        #         haproxy_backends[role]['zones'][availability_zone] = {}
-        #         haproxy_backends[role]['zones'][availability_zone]['nodes'] = {}
-        #       end
-        #
-        #       haproxy_backends[role]['zones'][availability_zone]['current'] = true if current_availability_zone == availability_zone
-        #
-        #       unless current_private_ip == private_ip
-        #         haproxy_backends[role]['zones'][availability_zone]['nodes'][id] = {}
-        #         haproxy_backends[role]['zones'][availability_zone]['nodes'][id]['ip'] = private_ip
-        #         Chef::Log.info("haproxy-ec2-discovery: Discovered node with ip '#{private_ip}', role '#{role}' and availability_zone '#{availability_zone}'")
-        #
-        #         if role == "share" || role == "allinone"
-        #           backend_options = "check cookie #{instanceName.split('-')[1]} inter 5000"
-        #         end
-        #
-        #         haproxy_backends[role]['zones'][availability_zone]['nodes'][id]['options'] = backend_options
-        #       else
-        #         Chef::Log.info("Skipping instance #{private_ip} as it's the current instance")
-        #         if role == "share" || role == "allinone"
-        #           haproxy_backends[role]['local_options'] = "check cookie #{instanceName.split('-')[1]} inter 5000"
-        #         end
-        #       end
-        #
-        #       if role == 'allinone'
-        #         allinone_local_share_options = haproxy_backends[role]['local_options']
-        #         allinone_local_options = "check inter 5000"
-        #         allinone_backup_options = "check inter 5000 backup"
-        #       end
-        #     end
-        #   end
-        # end
-        # end
-        #
-        # if haproxy_backends['allinone']
-        # haproxy_backends['alfresco']['zones'] = haproxy_backends['allinone']['zones']
-        # haproxy_backends['share']['zones'] = haproxy_backends['allinone']['zones']
-        # haproxy_backends['solr']['zones'] = haproxy_backends['allinone']['zones']
-        # end
-        #
-        # # AOS backend is hosted by alfresco, so it inherits same haproxy configurations
-        # if haproxy_backends['alfresco'] and haproxy_backends['aos_vti'] and haproxy_backends['aos_root']
-        # haproxy_backends['aos_vti']['zones'] = haproxy_backends['alfresco']['zones']
-        # haproxy_backends['aos_root']['zones'] = haproxy_backends['alfresco']['zones']
-        # end
-        # Chef::Log.info("Haproxy backends: #{haproxy_backends}")
+        return query_tag_filter
       end
 
-      private
-
-      def run_cmd(command)
-        cmd = shell_out!(command, {:returns => [0,2]})
-        cmd.error!
+      def getSubAttributeStr(node,path)
+        return getSubAttribute(node,path.split('/'))
       end
 
-      def getSubAttributeArray(node,path_array)
-        # puts "[DEBUG] getSubAttributeArray(#{node},#{path_array}) \n"
+      def getSubAttribute(node,path_array)
+        # puts "[DEBUG] getSubAttribute(#{node},#{path_array}) \n"
         unless node
           Application.fatal!("Commons::Helper.getSubAttribute failed! node is null while trying to resolve attribute #{path_array}")
         end
 
-        return node if node.kind_of?(String)
-
-        if path_array and path_array.size() > 0
-          return getSubAttributeArray(node[path_array[0]], path_array[1..-1])
+        if path_array and path_array.size() == 0
+          return node
+        else
+          return getSubAttribute(node[path_array[0]], path_array[1..-1])
         end
       end
 
-      def getSubAttribute(node,path)
-        # puts "[DEBUG] getSubAttribute(#{node},#{path}) \n"
-        ret = path ? getSubAttributeArray(node,path.split('/')) : nil
+      def stringToArray(string)
+        return string.split(',') if string.include? ','
+        return nil
+      end
+
+      def setFacetAttribute(node,path_array,value)
+        # puts "[DEBUG] setFacetAttribute(#{node.to_json},#{path_array},#{value}) \n"
+        unless node and path_array
+          Application.fatal!("Commons::Helper.setFacetAttribute failed! node or path_array is null while trying to set value #{value} on attribute #{path_array}")
+        end
+        attribute_group_name = path_array[0]
+        facet_value = value[path_array[0]]
+        facet_value_array = stringToArray(facet_value)
+        if path_array.size() == 1
+          if facet_value_array
+            facet_value_array.each do |facet_value_item|
+              node[attribute_group_name] = {} unless node[attribute_group_name]
+              node[attribute_group_name][facet_value_item] = value
+            end
+          else
+            node[attribute_group_name] = {} unless node[attribute_group_name]
+            node[attribute_group_name][facet_value] = value
+          end
+        else
+          if facet_value_array
+            facet_value_array.each do |facet_value_item|
+              node[attribute_group_name] = {} unless node[attribute_group_name]
+              node[attribute_group_name][facet_value_item] = {} unless node[attribute_group_name][facet_value_item]
+              setFacetAttribute(node[attribute_group_name][facet_value_item], path_array[1..-1],value)
+            end
+          else
+            node[attribute_group_name] = {} unless node[attribute_group_name]
+            node[attribute_group_name][facet_value] = {} unless node[attribute_group_name][facet_value]
+            setFacetAttribute(node[attribute_group_name][facet_value], path_array[1..-1],value)
+          end
+        end
+      end
+
+      def getTagValue(input_tags_array, tag_value_match)
+        input_tags_array.each do |tag|
+          if tag['Key'] == tag_value_match
+            return tag['Value']
+          end
+        end
       end
     end
   end
