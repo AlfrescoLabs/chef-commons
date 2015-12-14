4964ff91ee9f007437b091cd32003d84666e763b
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-14 17:26:18 +0100

    changed docs

--- attributes/ec2-tagging.rb
+++ attributes/ec2-tagging.rb
@@ -1,4 +1,4 @@
 # Example of tag creation for the current box
-# default['commons']['ec2_tags']['roles'] = 'alfresco,share,solr'
+# default['commons']['ec2_tags']['roles'] = 'role1,role2'
 # default['commons']['ec2_tags']['instance_name'] = 'my-instance-name1'
 # default['commons']['ec2_tags']['jvm_route'] = 'n1'
