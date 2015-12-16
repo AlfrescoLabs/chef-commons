f1a85e40c50b3d7344dd66ddc37670d9e55728d7
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-16 18:12:11 +0100

    fighting with idempotency, take 1

--- recipes/logstash-discovery.rb
+++ recipes/logstash-discovery.rb
@@ -1,4 +1,5 @@
 commons_logstash_discovery 'discover' do
+  config node['commons']['ec2-discovery']
   action :run
   only_if { node['commons']['logstash'] and node['commons']['logstash']['ec2'] and node['commons']['logstash']['ec2']['run_discovery'] }
 end
--- resources/logstash_discovery.rb
+++ resources/logstash_discovery.rb
@@ -1,33 +1,36 @@
 property :enable_discovery, kind_of: [TrueClass, FalseClass], default: false
+property :config, required: true
+property :ouput, default: []
 
 load_current_value do
   if ::File.exist?('/etc/logstash-forwarder.conf')
+    # Run EC2 discovery
+    ec2_discovery = Ec2Discovery.discover(:config)
+    logstash_servers = []
+    ec2_discovery.each do |serverItem,server|
+      Chef::Log.info("Adding logstash server: #{server}")
+      logstash_servers << server['ip']
+    end
+    Chef::Log.info("Logstash servers found: #{logstash_servers}")
+
+    output logstash_servers
     enable_discovery true
   end
 end
 
 action :run do
-  logstash_servers = []
+  load_current_value :output do
+    replace_or_add "setup_logstash_servers" do
+      path "/etc/logstash-forwarder.conf"
+      pattern "\"servers\": "
+      line "\"servers\": #{output.to_json}"
+      notifies :restart, 'service[logstash-forwarder]', :delayed
+      only_if { :enable_discovery }
+    end
 
-  # Run EC2 discovery
-  ec2_discovery_output = Ec2Discovery.discover(node['commons']['ec2_discovery'])
-
-  ec2_discovery_output.each do |serverItem,server|
-    Chef::Log.info("Adding logstash server: #{server}")
-    logstash_servers << server['ip']
-  end
-  Chef::Log.info("Logstash servers found: #{logstash_servers}")
-
-  replace_or_add "setup_logstash_servers" do
-    path "/etc/logstash-forwarder.conf"
-    pattern "\"servers\": "
-    line "\"servers\": #{logstash_servers.to_json}"
-    notifies :restart, 'service[logstash-forwarder]', :delayed
-    only_if { :enable_discovery }
-  end
-
-  service 'logstash-forwarder' do
-    action :nothing
-    only_if "service logstash-forwarder status"
+    service 'logstash-forwarder' do
+      action :nothing
+      only_if "service logstash-forwarder status"
+    end
   end
 end
