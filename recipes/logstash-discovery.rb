689b2d437d5e6e9b63d56059376cb55377fe3936
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-16 18:14:31 +0100

    fixed typo

--- recipes/logstash-discovery.rb
+++ recipes/logstash-discovery.rb
@@ -1,5 +1,5 @@
 commons_logstash_discovery 'discover' do
-  config node['commons']['ec2-discovery']
+  config node['commons']['ec2_discovery']
   action :run
   only_if { node['commons']['logstash'] and node['commons']['logstash']['ec2'] and node['commons']['logstash']['ec2']['run_discovery'] }
 end
