6b0b805ef74c474cc0226373f1d79a099b37ae38
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-17 00:46:26 +0100

    fixed chef-client-cron command

--- attributes/logstash.rb
+++
@@ -1,4 +0,0 @@
-default['commons']['logstash']['discovery-chef-json'] = "/etc/chef/logstash-discovery.json"
-default['commons']['logstash']['ec2_role_name'] = 'logstash'
-default['commons']['logstash']['run_discovery'] = false
-default['commons']['logstash']['discovery-cron'] = "*/5 * * * *"
