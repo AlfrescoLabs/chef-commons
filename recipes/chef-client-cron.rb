20caf4c14ba363af50e56e96227daedad080b35f
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-12-27 20:02:02 +0100

    fix directory creation for chef client cron recipe

--- recipes/chef-client-cron.rb
+++ recipes/chef-client-cron.rb
@@ -7,7 +7,7 @@ if cron_name and chef_json_cookbook and chef_json_source
   chef_json_path = "#{node['commons']['chef-client-cron']['chef_json_path_prefix']}/#{cron_name}.json"
   node.default['commons']['chef-client-cron']['chef_json_path'] = chef_json_path
 
-  directory chef_client_cron_path do
+  directory File.dirname(chef_client_cron_path) do
     recursive true
     action :create
   end
@@ -16,7 +16,7 @@ if cron_name and chef_json_cookbook and chef_json_source
     source 'cron/chef-client.cron.erb'
   end
 
-  directory chef_json_path do
+  directory File.dirname(chef_json_path) do
     recursive true
     action :create
   end
