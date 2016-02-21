3919b9d2223599c4fa1477795e79972035206622
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2016-02-21 12:57:53 +0100

    fixed chef-code recipe

--- recipes/chef-code.rb
+++ recipes/chef-code.rb
@@ -1,6 +1,25 @@
-default['commons']['cookbook_path'] = '/etc/chef/cookbooks'
-default['commons']['data_bag_path'] = '/etc/chef/data_bags'
+cookbook_path = node['commons']['cookbook_path']
+source_cookbook_path = node['commons']['source_cookbook_path']
+if source_cookbook_path
+  directory cookbook_path do
+    action :create
+  end
+  execute "create-#{cookbook_path}" do
+    command "rm -rf #{cookbook_path} ; cp -rf #{source_cookbook_path} #{cookbook_path}"
+    # Force override
+    # not_if "test -d #{cookbook_path}"
+  end
+end
 
-# Define these Chef attributes to enable the recipe behaviour
-# default['commons']['source_cookbook_path'] = '/tmp/kitchen/cookbooks'
-# default['commons']['source_data_bag_path'] = '/tmp/kitchen/data_bags'
+data_bag_path = node['commons']['data_bag_path']
+source_data_bag_path = node['commons']['source_data_bag_path']
+if source_data_bag_path
+  directory data_bag_path do
+    action :create
+  end
+  execute "create-#{data_bag_path}" do
+    command "rm -rf #{data_bag_path} ; cp -rf #{source_data_bag_path} #{data_bag_path}"
+    # Force override
+    # not_if "test -d #{data_bag_path}"
+  end
+end
