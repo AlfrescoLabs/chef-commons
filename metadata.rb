14184f3b8ff269b6f4d0eff35b06a6f5188a761b
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2016-03-17 15:59:10 +0100

    fixed foodcritic

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          "Apache 2.0"
 description      "Installs Alfresco Community and Enterprise Edition."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.0.1"
+issues_url ""
+source_url ""
 
 # Community cookbooks
 depends "python", ">= 1.4.6"
