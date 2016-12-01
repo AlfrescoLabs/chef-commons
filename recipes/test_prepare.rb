24d0240000373f16c004df2e75b65ddd3303a964
Enzo Rivello <vincenzo.rivello@gmail.com>
2016-12-01 14:25:27 +0000

    linting test_prepare

--- recipes/test_prepare.rb
+++ recipes/test_prepare.rb
@@ -1,5 +1,5 @@
-ruby_block "Save node attributes" do
+ruby_block 'Save node attributes' do
   block do
-    File.write("/tmp/kitchen_chef_node.json", node.to_json)
+    File.write('/tmp/kitchen_chef_node.json', node.to_json)
   end
 end
